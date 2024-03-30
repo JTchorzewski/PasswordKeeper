8352f901abea4bbd67ecf6b5fd53236b327ee4b1
Jakub Tchórzewski <jakub.tchorzewski23@gmail.com>
2024-03-30 10:26:05 +0100

    new

---
+++ PasswordKeeper/Device/DeviceTemplate.cs
@@ -0,0 +1,99 @@
+﻿public class DeviceTemplate
+{
+
+    public void UseOfDevice(string filePath, int optionDevice, string securePassword, List<string> correctSecurePassword, List<string> devicePasswords, ITextualRepository fileOperator)
+    {
+        do
+        {
+            Console.WriteLine("What do you want to do with your passwords: ");
+            Console.WriteLine("1. Show all passwords");
+            Console.WriteLine("2. Add new password");
+            Console.WriteLine("3. Remove password");
+            Console.WriteLine("4. Exit to previous page");
+
+            var chooseDevice = Console.ReadLine();
+            var IsParsableDevice = int.TryParse(chooseDevice, out optionDevice);
+            Console.WriteLine();
+            if(IsParsableDevice)
+            {
+                if (optionDevice == 1)
+                {
+                    do
+                    {
+                        Console.WriteLine("To see passwords, at first you have to put your secure password:");
+                        securePassword = Console.ReadLine();
+                        Console.WriteLine();
+
+                        if (securePassword == correctSecurePassword[0])
+                        {
+                            int passwordId = 1;
+                            foreach (var devicePassword in devicePasswords)
+                            {
+                                Console.WriteLine($"{passwordId}. {devicePassword}");
+                                passwordId++;
+                            }
+                            Console.WriteLine();
+                            break;
+                        }
+                        else if (securePassword != correctSecurePassword[0] && securePassword != "e" && securePassword != "E")
+                        {
+                            Console.WriteLine("Password was incorrect. Try again or type E to exit to previous menu.");
+                        }
+                    } while (securePassword != "e" && securePassword != "E");
+                }
+                else if (optionDevice == 2)
+                {
+                    Console.WriteLine("Write a Password you want to add:");
+                    var newPassword = Console.ReadLine();
+                    Console.WriteLine("Write a category, website, app for what this password is");
+                    var newPasswordCategory = Console.ReadLine();
+                    newPassword = $"{newPassword} ({newPasswordCategory})";
+                    devicePasswords.Add(newPassword);
+                    fileOperator.Write(filePath, devicePasswords);
+                }
+                else if (optionDevice == 3)
+                {
+
+                    do
+                    {
+                        Console.WriteLine("To see passwords, at first you have to put your secure password:");
+                        securePassword = Console.ReadLine();
+
+                        if (securePassword == correctSecurePassword[0])
+                        {
+                            Console.WriteLine("Your passwords list:");
+                            int passwordId = 1;
+                            foreach (var devicePassword in devicePasswords)
+                            {
+                                Console.WriteLine($"{passwordId}. {devicePassword}");
+                                passwordId++;
+                            }
+                            Console.WriteLine("Type a id of password you want to delete:");
+                            var passwordToDelete = Console.ReadLine();
+                            var passwordToDeleteId = int.Parse(passwordToDelete);
+                            devicePasswords.RemoveAt(passwordToDeleteId - 1);
+                            fileOperator.Write(filePath, devicePasswords);
+                            break;
+                        }
+                        else if (securePassword != correctSecurePassword[0] && securePassword != "e" && securePassword != "E")
+                        {
+                            Console.WriteLine("Password was incorrect. Try again or type E to exit to previous menu.");
+                        }
+                    } while (securePassword != "e" && securePassword != "E");
+                }
+                else
+                {
+                    Console.WriteLine("You have put wrong option. Try again.");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have put wrong option. Try again.");
+                Console.WriteLine();
+            }
+            
+        } while (optionDevice != 4);
+        
+    }
+}
--- PasswordKeeper/Program.cs
+++ PasswordKeeper/Program.cs
@@ -1,10 +1,15 @@
 ﻿string choose;
 bool IsParsable;
 int option;
-int optionPhone;
+int optionPhone = 0;
 int optionDevice;
-int optionComputer;
-int optionOthers;
+int optionComputer = 0;
+int optionOthers = 0;
+var deviceTemplate = new DeviceTemplate();
+var securePasswordCheck = new SecurePassword();
+var fileOperator = new StringsTextualRepository();
+var correctSecurePassword = fileOperator.Read("securePassword.txt");
+string securePassword = "";
 
 do
 {
@@ -37,176 +42,19 @@ do
 
             if (optionDevice == 1)
             {
-                var fileOperator = new StringsTextualRepository();
-                var phonePasswords = fileOperator.Read("phone.txt");
-                do
-                {
-                    Console.WriteLine("What do you want to do with your passwords: ");
-                    Console.WriteLine("1. Show all passwords");
-                    Console.WriteLine("2. Add new password");
-                    Console.WriteLine("3. Remove password");
-                    Console.WriteLine("4. Exit to previous page");
-                    
-                    var choosePhone = Console.ReadLine();
-                    string securePassword;
-                    var correctSecurePassword = fileOperator.Read("securePassword.txt");
-                    var IsParsablePhone = int.TryParse(choosePhone, out optionPhone);
-                    Console.WriteLine();
-                    if (optionPhone == 1)
-                    {
-                        do
-                        {
-                            Console.WriteLine("To see passwords, at first you have to put your secure password:");
-                            securePassword = Console.ReadLine();
-
-
-                            if (securePassword == correctSecurePassword[0])
-                            {
-                                int passwordId = 1;
-                                foreach (var phonePassword in phonePasswords)
-                                {
-                                    Console.WriteLine($"{passwordId}. {phonePassword}");
-                                    passwordId++;
-                                }
-                                break;
-                            }
-                            else if(securePassword != correctSecurePassword[0] && securePassword != "e" && securePassword != "E")
-                            {
-                                Console.WriteLine("Password was incorrect. Try again or type E to exit to previous menu.");
-                            }
-                        } while (securePassword != "e" && securePassword != "E");
-                    }
-                    else if (optionPhone == 2)
-                    {
-                        Console.WriteLine("Write a Password you want to add:");
-                        var newPassword = Console.ReadLine();
-                        Console.WriteLine("Write a category, website, app for what this password is");
-                        var newPasswordCategory = Console.ReadLine();
-                        newPassword = $"{newPassword} ({newPasswordCategory})";
-                        phonePasswords.Add(newPassword);
-                        fileOperator.Write("phone.txt", phonePasswords);
-                    }
-                    else if (optionPhone == 3)
-                    {
-                        Console.WriteLine("Your passwords list:");
-                        int passwordId = 1;
-                        foreach (var phonePassword in phonePasswords)
-                        {
-                            Console.WriteLine($"{passwordId}. {phonePassword}");
-                            passwordId++;
-                        }
-                        Console.WriteLine("Type a id of password you want to delete:");
-                        var passwordToDelete = Console.ReadLine();
-                        var passwordToDeleteId = int.Parse(passwordToDelete);
-                        phonePasswords.RemoveAt(passwordToDeleteId - 1);
-                        fileOperator.Write("phone.txt", phonePasswords);
-                    }
-                    Console.WriteLine();
-                } while (optionPhone != 4);
+                var phonePasswords = fileOperator.Read("phone.txt");                  
+                deviceTemplate.UseOfDevice("phone.txt", optionPhone, securePassword, correctSecurePassword, phonePasswords, fileOperator);            
             }
             else if (optionDevice == 2)
             {
-                var fileOperator = new StringsTextualRepository();
                 var computerPasswords = fileOperator.Read("computer.txt");
-                do
-                {
-                    Console.WriteLine("What do you want to do with your passwords: ");
-                    Console.WriteLine("1. Show all passwords");
-                    Console.WriteLine("2. Add new password");
-                    Console.WriteLine("3. Remove password");
-                    Console.WriteLine("4. Exit to previous page");
-
-                    var chooseComputer = Console.ReadLine();
-                    var IsParsableComputer = int.TryParse(chooseComputer, out optionComputer);
-                    Console.WriteLine();
-                    if (optionComputer == 1)
-                    {
-                        int passwordId = 1;
-                        foreach (var computerPassword in computerPasswords)
-                        {
-                            Console.WriteLine($"{passwordId}. {computerPassword}");
-                            passwordId++;
-                        }
-                    }
-                    else if (optionComputer == 2)
-                    {
-                        Console.WriteLine("Write a Password you want to add:");
-                        var newPassword = Console.ReadLine();
-                        Console.WriteLine("Write a category, website, app for what this password is");
-                        var newPasswordCategory = Console.ReadLine();
-                        newPassword = $"{newPassword} ({newPasswordCategory})";
-                        computerPasswords.Add(newPassword);
-                        fileOperator.Write("computer.txt", computerPasswords);
-                    }
-                    else if (optionComputer == 3)
-                    {
-                        Console.WriteLine("Your passwords list:");
-                        int passwordId = 1;
-                        foreach (var computerPassword in computerPasswords)
-                        {
-                            Console.WriteLine($"{passwordId}. {computerPassword}");
-                            passwordId++;
-                        }
-                        Console.WriteLine("Type a id of password you want to delete:");
-                        var passwordToDelete = Console.ReadLine();
-                        var passwordToDeleteId = int.Parse(passwordToDelete);
-                        computerPasswords.RemoveAt(passwordToDeleteId - 1);
-                        fileOperator.Write("computer.txt", computerPasswords);
-                    }
-                    Console.WriteLine();
-                } while (optionComputer != 4);
+                deviceTemplate.UseOfDevice("computer.txt", optionComputer, securePassword, correctSecurePassword, computerPasswords, fileOperator);
             }
             else if (optionDevice == 3)
-            {
-                var fileOperator = new StringsTextualRepository();
+            {                
                 var othersPasswords = fileOperator.Read("others.txt");
-                do
-                {
-                    Console.WriteLine("What do you want to do with your passwords: ");
-                    Console.WriteLine("1. Show all passwords");
-                    Console.WriteLine("2. Add new password");
-                    Console.WriteLine("3. Remove password");
-                    Console.WriteLine("4. Exit to previous page");
-
-                    var chooseOthers = Console.ReadLine();
-                    var IsParsableOthers = int.TryParse(chooseOthers, out optionOthers);
+                    deviceTemplate.UseOfDevice("others.txt", optionOthers, securePassword, correctSecurePassword, othersPasswords, fileOperator);
                     Console.WriteLine();
-                    if (optionOthers == 1)
-                    {
-                        int passwordId = 1;
-                        foreach (var othersPassword in othersPasswords)
-                        {
-                            Console.WriteLine($"{passwordId}. {othersPassword}");
-                            passwordId++;
-                        }
-                    }
-                    else if (optionOthers == 2)
-                    {
-                        Console.WriteLine("Write a Password you want to add:");
-                        var newPassword = Console.ReadLine();
-                        Console.WriteLine("Write a category, website, app for what this password is");
-                        var newPasswordCategory = Console.ReadLine();
-                        newPassword = $"{newPassword} ({newPasswordCategory})";
-                        othersPasswords.Add(newPassword);
-                        fileOperator.Write("others.txt", othersPasswords);
-                    }
-                    else if (optionOthers == 3)
-                    {
-                        Console.WriteLine("Your passwords list:");
-                        int passwordId = 1;
-                        foreach (var othersPassword in othersPasswords)
-                        {
-                            Console.WriteLine($"{passwordId}. {othersPassword}");
-                            passwordId++;
-                        }
-                        Console.WriteLine("Type a id of password you want to delete:");
-                        var passwordToDelete = Console.ReadLine();
-                        var passwordToDeleteId = int.Parse(passwordToDelete);
-                        othersPasswords.RemoveAt(passwordToDeleteId - 1);
-                        fileOperator.Write("others.txt", othersPasswords);
-                    }
-                    Console.WriteLine();
-                } while (optionOthers != 4);
             }
             Console.WriteLine();
         } while(optionDevice != 4);
@@ -214,30 +62,7 @@ do
 
     else if (option == 2)
     {
-        var fileOperator = new StringsTextualRepository();
-        var securePassword = fileOperator.Read("securePassword.txt");
-
-        Console.WriteLine(@"To change your secure password, first you have to
-type present secure password:");
-        string securePasswordByUser;
-        do
-        {
-            securePasswordByUser = Console.ReadLine();
-            if (securePasswordByUser == securePassword[0])
-            {
-                Console.WriteLine("Password is correct!");
-                Console.WriteLine("Type your new secure password:");
-                var newSecurePassword = Console.ReadLine();
-                securePassword[0] = newSecurePassword;
-                fileOperator.Write("securePassword.txt", securePassword);
-                break;
-            }
-            else if(securePasswordByUser != securePassword[0] && securePasswordByUser != "E" && securePasswordByUser != "e")
-            {
-                Console.WriteLine("You put wrong password");
-                Console.WriteLine($"Try again or press E to back to previous menu");
-            }
-        } while (securePasswordByUser != "E" && securePasswordByUser != "e");
+        securePasswordCheck.SecurePasswordChange(fileOperator, "securePassword.txt");
     }
 
     else if(option != 1 && option != 2 && option != 3)
@@ -247,18 +72,3 @@ type present secure password:");
     Console.WriteLine();
 } while (option != 3);
 
-public class StringsTextualRepository
-{
-    private static readonly string Separator = Environment.NewLine;
-
-    public List<string> Read(string filePath)
-    {
-        var fileContents = File.ReadAllText(filePath);
-        return fileContents.Split(Separator).ToList();
-    }
-
-    public void Write(string filePath, List<string> strings)
-    {
-        File.WriteAllText(filePath, string.Join(Separator, strings));
-    }
-}
